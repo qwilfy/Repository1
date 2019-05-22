708222204f1055b785722f827feacefc1d10966f
Vadim Vassiltsenko <opilane@tthk.ee>
2019-05-22 09:31:53 +0300

    Exercise java

---
+++ kalkScript.js
@@ -0,0 +1,29 @@
+function hindKokku(){
+	kogus=document.getElementById("kogus");
+	vastus=document.getElementById("hind");
+	valuta=document.getElementById("valuta").value;
+	
+	alert("Convert to" + " " + valuta);
+	
+	if (valuta=="BTC"){
+	vastus.innerHTML= kogus.value + " eur" + ", saa saad" + " " + valuta + " " +
+	
+	(kogus.value/5436).toFixed(2);
+	}
+	else if (valuta=="ETH"){
+	vastus.innerHTML= kogus.value + " eur" + ", saa saad" + " " + valuta + " " +
+	
+	(kogus.value/173).toFixed(2);
+	}
+	else if (valuta=="LTC"){
+	vastus.innerHTML= kogus.value + " eur" + ", saa saad" + " " + valuta + " " +
+	
+	(kogus.value/45).toFixed(2);
+	}
+	else if (valuta=="XMR"){
+	vastus.innerHTML= kogus.value + " eur" + ", saa saad" + " " + valuta + " " +
+	
+	(kogus.value/90).toFixed(2);
+	}
+	
+}
